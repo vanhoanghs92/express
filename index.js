cf40a61760351c1b6ce6f11c15a0e39daf98ef73
Tran Van Hoang <alan.tran@kyberosc.com>
2019-09-29 15:47:34 +0700

    update section 8

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const shortid = require('shortid');
 
-db = low(adapter);
+const userRoutes = require('./routes/user.route');
 
-db.defaults({ users: [] }).write();
+const port = 8090;
+
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -22,46 +19,8 @@ app.get('/', function (req, res) {
     });
 });
 
-app.get('/users', (req, res) => {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/search', (req, res) => {
-    let name = req.query.name;
-    const users =  db.get('users').value();
-
-    const matchUsers = users.filter(function (user) {
-        return user.name.toLowerCase().indexOf(name) !== -1;
-    });
-
-    res.render('users/index', {
-        users: matchUsers,
-        value: name
-    });
-});
-
-app.get('/users/:id', function(req, res) {
-  const id = parseInt(req.params.id);
-//   console.log(id);
-  const user = db.get('users').find({ id: id }).value();
-  console.log(user);
-  res.render('users/view', {
-      user: user
-  });
-});
-
-app.get('/users/create', function (req, res) {
-    res.render('users/create');
-});
-
-app.post('/users/create', function (req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
+app.use('/users', userRoutes);
 
 app.listen(8090, function () {
-    console.log('app listen on port 8090');
+    console.log(`router listen on port ${port}`);
 });
